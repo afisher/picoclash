8c593dda9ea01a8a86b43ae810d6b50128201570
Ashley Fisher <fish.ash@gmail.com>
2012-01-30 21:05:23 -0800

    First commit

---
+++ Archer.h
@@ -0,0 +1,8 @@
+#include "SDL/SDL.h"
+#include "SDL/SDL_image.h"
+
+class Healer: public Character {
+    private:
+        static SDL_Surface* image;
+}
+
---
+++ Character.h
@@ -0,0 +1,12 @@
+#include "SDL/SDL.h"
+#include "SDL/SDL_image.h"
+#include <string>
+
+class Character {
+    private:
+        int mobility;
+        int health;
+        int strength;
+
+        std::string name;
+}
