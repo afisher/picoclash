c111442de3e1b59f9c9a40b331ce05e701cc682c
Ashley Fisher <fish.ash@gmail.com>
2012-06-14 23:52:20 -0700

    Added license.

--- Picoclash.cpp
+++ Picoclash.cpp
@@ -1,3 +1,21 @@
+/*
+ * This file is a part of Picoclash
+ * Copyright (C) 2012 Ashley S. Fisher
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "Header.h"
 
 using namespace std;
@@ -138,7 +156,6 @@ int main(int argc, char* args[]) {
 
     // make the map selector
     MapSelector* selector = new MapSelector();
-    Util::update_screen(selector->get_surface(), screen);
 
     bool game_started = false;
 
